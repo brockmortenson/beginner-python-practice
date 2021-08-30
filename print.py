cc45349865f415a509f53aebdf311a088573e81d
Brock Mortenson <mortensonbrock@gmail.com>
2021-08-30 13:55:07 -0600

    first commit

---
+++ print.py
@@ -0,0 +1,7 @@
+x = ['array', 'string in array', 5, True]
+y = [17, 'string', False]
+new = x + y
+if new.count('string') > 1 : print('Two instances')
+else : print('1 or less instances')
+
+# print(new)
